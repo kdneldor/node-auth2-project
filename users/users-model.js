7ad227a1e30781d651d71eef4c761edbc07864f9
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-11-02 11:47:45 -0600

    Most of project is written, ran into server issue.

---
+++ users/users-model.js
@@ -0,0 +1,25 @@
+const db = require("../database/config");
+
+async function add(user) {
+  const [id] = await db("users").insert(user);
+  return findById(id);
+}
+
+function find() {
+  return db("users as u")
+    .innerJoin("roles as r", "r.id", "u.role_id")
+    .select("u.id", "u.username", "r.name as role");
+}
+
+function findByUsername(username) {
+  return db("users as u")
+    .innerJoin("roles as r", "r.id", "u.role_id")
+    .where("u.username", username)
+    .first("u.id", "u.username", "u.password", "r.name as role");
+}
+
+module.exports = {
+  add,
+  find,
+  findByUsername,
+};
